5e8c905939d78583c9b31324a835eedb637e4c62
brandonvidal578 <brandon.vidal578@pascualbravo.edu.co>
2026-02-27 21:58:12 -0500

    Updated function

--- wp-content/themes/moira-child/functions.php
+++ wp-content/themes/moira-child/functions.php
@@ -3,52 +3,23 @@
  * Moira Child Theme Functions
  */
 
-add_action( 'wp_enqueue_scripts', 'moira_child_enqueue_styles' );
-function moira_enqueue_styles() {
+// 1. CARGA DE ESTILOS (Corregido)
+add_action( 'wp_enqueue_scripts', 'moira_child_enqueue_styles' ); // El nombre aquí...
+
+function moira_child_enqueue_styles() { // ...debe ser IGUAL al de aquí.
+    // Carga el estilo del tema padre
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    // Carga el estilo del tema hijo
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
 }
 
-// BLINDAJE DE SEGURIDAD: OCULTAR HUELLAS DE WORDPRESS
-remove_action('wp_head', 'wp_generator');
-
-// Se restringe el acceso a la REST API para usuarios no autenticados
-add_filter( 'rest_authentication_errors', function( $result ) {
-    if ( ! empty( $result ) ) {
-        return $result;
-    }
-    if ( ! is_user_logged_in() ) {
-        return new WP_Error( 'rest_not_logged_in', 'Acceso restringido a Moira.', array( 'status' => 401 ) );
-    }
-    return $result;
-});
-
-// PROTECCIÓN DE CONTENIDO: REDIRECCIÓN DE SUSCRIPTORES
-add_action( 'admin_init', 'moira_restrict_admin_access' );
-function moira_restrict_admin_access() {
-    if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) return;
-    
-    if ( current_user_can( 'subscriber' ) ) {
-        wp_safe_redirect( home_url('/mi-cuenta/') );
+// 2. RESTRICCIÓN DE ACCESO (Lo que añadimos anteriormente)
+add_action( 'template_redirect', 'moira_restrict_member_content' );
+function moira_restrict_member_content() {
+    $slug_exclusivo = 'miembros-exclusivos';
+    if ( is_page( $slug_exclusivo ) && ! is_user_logged_in() ) {
+        wp_safe_redirect( home_url() );
         exit;
     }
 }
-
-// PERSONALIZACIÓN DE MARCA: LOGO EN EL LOGIN
-add_action( 'login_enqueue_scripts', 'moira_custom_login_logo' );
-function moira_custom_login_logo() { ?>
-    <style type="text/css">
-        #login h1 a, .login h1 a {
-            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.png);
-            height: 100px;
-            width: 300px;
-            background-size: contain;
-            background-repeat: no-repeat;
-        }
-        body.login { background-color: #F2EFDC; }
-        .login #wp-submit { background: #B65835; border-color: #B65835; }
-    </style>
-<?php }
-
-// DESACTIVAR XML-RPC
-add_filter( 'xmlrpc_enabled', '__return_false' );
